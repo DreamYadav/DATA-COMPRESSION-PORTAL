7934b42814d1adbd5cb5e8bfdc0d08d0e03a51b8
DreamYadav <dreampyadav@gmail.com>
2025-06-25 09:05:34 +0530

    Added deployment config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/DATA-COMPRESSION-PORTAL/", // 👈 this is the important line
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
